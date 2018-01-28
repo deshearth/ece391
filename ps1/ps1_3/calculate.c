a90993d64aeffd10b900cc6e5d4f2394db613133
deshearth <ychu26@illinois.edu>
2018-01-28 02:54:57 -0600

    c file editted by clz, seg fault

---
+++ ps1/ps1_3/calculate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calculate(uint32_t operation, int arg1, int arg2) {
+	if (operation > 3) 
+		return 0xECE391;
+	else if (operation == 0) {
+		return *((int*)(arg2 * 8 + arg - 4)) + arg2;
+	}
+	else if (operation == 1) {
+		if (arg1 + 5 < arg2) return arg1;
+		else return arg2;
+	}
+	else if (operation == 2) {
+		return arg2 >> (arg1 & 0x00000011);
+	}
+	else if (operation == 3) {
+		return arg2 + arg1 * 2 + 27;
+	}
+}
+
+int main() {
+	printf(calculate(0,1,2));
+}
