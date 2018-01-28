7f4b404ebe7e22c20134b9f9556f1d1ff0a72fe6
Yifeng Chu <melancholymirth@gmail.com>
2018-01-28 03:06:00 -0600

    seg fault should be fine since op0 access some unknown mem

--- ps1/ps1_3/calculate.c
+++ ps1/ps1_3/calculate.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int calculate(uint32_t operation, int arg1, int arg2) {
 	if (operation > 3) 
 		return 0xECE391;
 	else if (operation == 0) {
-		return *((int*)(arg2 * 8 + arg - 4)) + arg2;
+		return *((int*)(arg2 * 8 + arg1 - 4)) + arg2;
+		//return 0;
 	}
 	else if (operation == 1) {
 		if (arg1 + 5 < arg2) return arg1;
@@ -19,5 +21,5 @@ int calculate(uint32_t operation, int arg1, int arg2) {
 }
 
 int main() {
-	printf(calculate(0,1,2));
+	printf("%d\n", calculate(0,1,2));
 }
