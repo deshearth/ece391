b7be64893339b50b495071b94e9616de3a8db573
Chu <ychu26@illinois.edu>
2018-02-09 15:02:01 -0600

    doc for ps2 added

--- ps1/ps1_2/main.c
+++ ps1/ps1_2/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-extern int calcSum_lz(int array[], int arrayLen);
+extern int calcSum_yf(int array[], int arrayLen);
 
 int main() {
 	int array[4] = {1,2,3,4};
 	int arrayLength = 4;
-	printf("%d\n", calcSum_lz(array, arrayLength));
+	printf("%d\n", calcSum_yf(array, arrayLength));
 	int arr2[6] = {-1,4,5,3,9,30};
-	int arrayLength_2 = 4;
-	printf("%d\n", calcSum_lz(arr2, arrayLength_2));
+	int arrayLength_2 = 6;
+	printf("%d\n", calcSum_yf(arr2, arrayLength_2));
 }
